bcaafb4925fdd05023b08992f8eacc99a7739d5a
AlexeyQQQ <leadterrier@gmail.com>
2023-11-21 20:57:12 +0300

    KS-1-2 first task solution

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val textWithThanks: String = "Thanks for the order!"
+    println(numberOfOrders)
+    println(textWithThanks)
+
+    var numberOfEmployees: Int = 2000
+//    println(numberOfEmployees)
+
+    numberOfEmployees -= 1
+    println(numberOfEmployees)
+}
