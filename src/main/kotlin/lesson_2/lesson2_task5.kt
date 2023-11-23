a6bc37899846d8cb667e29ef25ed1dcd1d1593c7
AlexeyQQQ <leadterrier@gmail.com>
2023-11-23 10:57:57 +0300

    KS-2-5 first task solution

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+    val sumFromClient = 70_000
+    val percentRate = 16.7
+    val depositPeriod = 20
+
+    val result = sumFromClient * (1 + percentRate / 100).pow(depositPeriod)
+    println(String.format("%.3f", result))
+}
