a787f803c3d6b24f7e2ecbb5ec4e0814a42b95cb
AlexeyQQQ <leadterrier@gmail.com>
2023-11-24 11:32:56 +0300

    KS-3-1 first task solution

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val userName = "John"
+
+    var greeting = "Good afternoon, $userName!"
+    println(greeting)
+
+    greeting = "Good evening, $userName!"
+    println(greeting)
+}
