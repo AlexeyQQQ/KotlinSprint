e06546b427656fcbcd6158d7b154d481df2d7071
AlexeyQQQ <leadterrier@gmail.com>
2023-11-24 16:07:10 +0300

    KS-4-4 first task solution

---
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -0,0 +1,17 @@
+package lesson_4
+
+fun main() {
+    val day = 5
+
+    val arms = day % 2 != 0
+    val legs = day % 2 == 0
+    val back = day % 2 == 0
+    val abs = day % 2 != 0
+
+    println("""
+        Упражнения для рук:    $arms
+        Упражнения для ног:    $legs
+        Упражнения для спины:  $back
+        Упражнения для пресса: $abs
+    """.trimIndent())
+}
