ded5f731f3f48c47531de68a69297507e4309833
AlexeyQQQ <leadterrier@gmail.com>
2023-11-25 18:45:13 +0300

    KS-3-4 corrected format

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -5,11 +5,11 @@ fun main() {
     var end = "E4"
     var moveNumber = 1
 
-    println("$start - $end ; $moveNumber")
+    println("[$start-$end;$moveNumber]")
 
     start = "D2"
     end = "D3"
     moveNumber += 2
 
-    println("$start - $end ; $moveNumber")
+    println("[$start-$end;$moveNumber]")
 }
