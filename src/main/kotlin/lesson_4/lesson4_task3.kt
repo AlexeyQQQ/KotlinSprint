dc873c1054e1a2ed3e8179111aa239d816e17c40
AlexeyQQQ <leadterrier@gmail.com>
2023-11-25 19:00:09 +0300

    KS-4-3 corrected boolean names

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,20 +1,20 @@
 package lesson_4
 
-const val SUNNY_WEATHER = true
-const val OPEN_TENT = true
+const val IS_SUNNY_WEATHER = true
+const val IS_AWNING_OPEN = true
 const val AIR_DAMP = 20
 const val WINTER = "Зима"
 
 fun main() {
-    val weather: Boolean = true
-    val tent: Boolean = true
+    val isSunny: Boolean = true
+    val isAwningOpen: Boolean = true
     val damp: Int = 20
     val season: String = "Зима"
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых? ${
-            weather == SUNNY_WEATHER
-                    && tent == OPEN_TENT
+            isSunny == IS_SUNNY_WEATHER
+                    && isAwningOpen == IS_AWNING_OPEN
                     && damp == AIR_DAMP
                     && season != WINTER
         }"
