28f1f518fc6fba9671890974e16aecf536eb4fd8
AlexeyQQQ <leadterrier@gmail.com>
2023-11-25 19:05:31 +0300

    KS-4-5 corrected boolean names

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,14 +1,14 @@
 package lesson_4
 
-const val DAMAGE = true
+const val IS_DAMAGE = true
 const val MIN_CREW = 55
 const val MAX_CREW = 70
 const val PROVISIONS = 50
-const val GOOD_WEATHER = true
+const val IS_GOOD_WEATHER = true
 
 fun main() {
     println("Наличие повреждений корпуса (true/false):")
-    val haveDamage = readln().toBoolean()
+    val isDamage = readln().toBoolean()
 
     println("Текущий состав экипажа (число):")
     val numberOfCrew = readln().toInt()
@@ -17,13 +17,13 @@ fun main() {
     val numberOfProvisions = readln().toInt()
 
     println("Благоприятность метеоусловий (true/false):")
-    val goodWeather = readln().toBoolean()
+    val isGoodWeather = readln().toBoolean()
 
-    val swimming = ((haveDamage != DAMAGE)
+    val swimming = ((isDamage != IS_DAMAGE)
             && (numberOfCrew in MIN_CREW..MAX_CREW)
             && (numberOfProvisions > PROVISIONS))
             || ((numberOfCrew == MAX_CREW)
-            && (goodWeather == GOOD_WEATHER)
+            && (isGoodWeather == IS_GOOD_WEATHER)
             && (numberOfProvisions >= PROVISIONS))
 
     println("Разрешение на плавание: $swimming")
