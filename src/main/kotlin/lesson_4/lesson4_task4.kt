00d43fec88089551d865c1947b5c532d2a1ecd58
AlexeyQQQ <leadterrier@gmail.com>
2023-11-27 10:40:47 +0300

    KS-4-4 corrected DRY

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,16 +2,12 @@ package lesson_4
 
 fun main() {
     val day = 5
-
-    val arms = day % 2 != 0
-    val legs = day % 2 == 0
-    val back = day % 2 == 0
-    val abs = day % 2 != 0
+    val isEvenDay = day % 2 == 0
 
     println("""
-        Упражнения для рук:    $arms
-        Упражнения для ног:    $legs
-        Упражнения для спины:  $back
-        Упражнения для пресса: $abs
+        Упражнения для рук:    ${!isEvenDay}
+        Упражнения для ног:    $isEvenDay
+        Упражнения для спины:  $isEvenDay
+        Упражнения для пресса: ${!isEvenDay}
     """.trimIndent())
 }
