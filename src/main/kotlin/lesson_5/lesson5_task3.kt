dc310ea83c70ff1398b7f67a786d7afadde38890
AlexeyQQQ <leadterrier@gmail.com>
2023-11-28 10:56:34 +0300

    KS-5-3 correct conditions

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -10,16 +10,12 @@ fun main() {
     val userNum2 = readln().toInt()
 
     when {
-        userNum1 == winNum1 ->
-            if (userNum2 == winNum2) println("Поздравляем! Вы выиграли главный приз!")
-            else println("Вы выиграли утешительный приз!")
+        (userNum1 == winNum1 && userNum2 == winNum2)
+                || (userNum1 == winNum2 && userNum2 == winNum1) -> println("Поздравляем! Вы выиграли главный приз!")
 
-        userNum1 == winNum2 ->
-            if (userNum2 == winNum1) println("Поздравляем! Вы выиграли главный приз!")
-            else println("Вы выиграли утешительный приз!")
+        (userNum1 == winNum1) || (userNum1 == winNum2) || (userNum2 == winNum1)
+                || (userNum2 == winNum2) -> println("Вы выиграли утешительный приз!")
 
-        userNum2 == winNum1 -> println("Вы выиграли утешительный приз!")
-        userNum2 == winNum2 -> println("Вы выиграли утешительный приз!")
         else -> println("Неудача!")
     }
 
