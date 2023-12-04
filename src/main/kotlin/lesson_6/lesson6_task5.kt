dcd7eb58904f2bd6a3b1ead5ae1f925fffeff151
AlexeyQQQ <leadterrier@gmail.com>
2023-12-04 12:01:03 +0300

    KS-6-5 first task solution

---
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -0,0 +1,46 @@
+package lesson_6
+
+fun main() {
+    var number1 = (1..9).random()
+    var number2 = (1..9).random()
+    var counter = 3
+
+    println(
+        "Для входа в приложение докажите, что вы не бот. У вас ${counter--} попытки."
+                + "\nРешите пример: $number1 + $number2 = ?"
+    )
+    var userAnswer = readln().toInt()
+
+    while ((userAnswer != number1 + number2) && (counter > 0)) {
+        number1 = (1..9).random()
+        number2 = (1..9).random()
+        println("Неверно! Кол-во оставшихся попыток: ${counter--}.\nРешите новый пример: $number1 + $number2 = ?")
+        userAnswer = readln().toInt()
+    }
+
+    if (userAnswer != number1 + number2) {
+        println("Доступ запрещен!")
+        return
+    } else {
+        println("Добро пожаловать!")
+    }
+
+    println("Создайте логин:")
+    val name = readln()
+    println("Создайте пароль:")
+    val password = readln()
+
+    println("Введите логин:")
+    var userName = readln()
+    println("Введите пароль:")
+    var userPassword = readln()
+
+    while (name != userName || password != userPassword) {
+        println("Данные введены неправильно. Попробуйте еще раз.\nВведите логин:")
+        userName = readln()
+        println("Введите пароль:")
+        userPassword = readln()
+    }
+
+    println("Авторизация прошла успешно!")
+}
