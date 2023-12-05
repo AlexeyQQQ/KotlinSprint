e3d6666945abe80db80829ab6b9b991d8a5f976a
AlexeyQQQ <leadterrier@gmail.com>
2023-12-05 13:00:11 +0300

    KS-6-5 removed re-check 2

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -1,44 +1,41 @@
 package lesson_6
 
 fun main() {
-    var number1 = (1..9).random()
-    var number2 = (1..9).random()
-    var counter = 3
+    var number1: Int
+    var number2: Int
+    var userAnswer: Int
+    var counterOfSolutions = 3
 
-    println(
-        "Для входа в приложение докажите, что вы не бот. У вас ${counter--} попытки."
-                + "\nРешите пример: $number1 + $number2 = ?"
-    )
-    var userAnswer = readln().toInt()
-
-    while (userAnswer != number1 + number2) {
-        if (counter > 0) {
+    println("Для входа в приложение докажите, что вы не бот.")
+    do {
+        if (counterOfSolutions > 0) {
             number1 = (1..9).random()
             number2 = (1..9).random()
-            println("Неверно! Кол-во оставшихся попыток: ${counter--}.\nРешите новый пример: $number1 + $number2 = ?")
+            println("У вас ${counterOfSolutions--} попытки. Решите пример: $number1 + $number2 = ?")
             userAnswer = readln().toInt()
         } else {
             println("Доступ запрещен!")
             return
         }
-    }
+    } while (userAnswer != number1 + number2)
 
     println("Добро пожаловать! Создайте логин:")
     val name = readln()
     println("Создайте пароль:")
     val password = readln()
 
-    println("Введите логин:")
-    var userName = readln()
-    println("Введите пароль:")
-    var userPassword = readln()
+    var userName: String
+    var userPassword: String
+    var counterOfLoginAttempts = 0
 
-    while (name != userName || password != userPassword) {
-        println("Данные введены неправильно. Попробуйте еще раз.\nВведите логин:")
+    do {
+        if (counterOfLoginAttempts > 0) println("Данные введены неправильно. Попробуйте еще раз.")
+        println("Введите логин:")
         userName = readln()
         println("Введите пароль:")
         userPassword = readln()
-    }
+        counterOfLoginAttempts++
+    } while (name != userName || password != userPassword)
 
     println("Авторизация прошла успешно!")
 }
