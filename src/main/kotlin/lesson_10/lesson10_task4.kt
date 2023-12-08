94586c48d5073743004cbab8f2ac5762df5b9f9d
AlexeyQQQ <leadterrier@gmail.com>
2023-12-08 11:52:45 +0300

    KS-10-4 changed variable

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -1,14 +1,14 @@
 package lesson_10
 
-private var counterUserVictory = 0
-
 fun main() {
+    var counterUserVictory = 0
+
     println("Начинаем игру!\n")
 
     while (true) {
         val playerThrow = throwDice()
         val computerThrow = throwDice()
-        outputResult(playerThrow, computerThrow)
+        counterUserVictory += outputResult(playerThrow, computerThrow)
 
         println("\nХотите бросить кости еще раз? Введите Да или Нет")
         if (readln() != "Да") break
@@ -19,16 +19,18 @@ fun main() {
 
 fun throwDice(): Int = (1..6).random()
 
-fun outputResult(playerThrow: Int, computerThrow: Int) {
+fun outputResult(playerThrow: Int, computerThrow: Int): Int {
     println("Игрок бросил кубики и получил: $playerThrow")
     println("Компьютер бросил кубики и получил: $computerThrow")
 
-    if (playerThrow == computerThrow) {
+    return if (playerThrow == computerThrow) {
         println("Ничья!")
+        0
     } else if (playerThrow > computerThrow) {
         println("Победил игрок!")
-        counterUserVictory++
+        1
     } else {
         println("Победил компьютер!")
+        0
     }
 }
