18d00130fa1c06948ba345753a2f2e8d04ced86f
AlexeyQQQ <leadterrier@gmail.com>
2023-12-18 22:15:06 +0300

    KS-21-4 correction

--- src/main/kotlin/lesson_21/lesson21_task4.kt
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -7,14 +7,14 @@ fun main() {
     val file = File("src/main/kotlin/lesson_21/file_task21.txt")
     file.createNewFile()
 
-    file.addText(file, "First")
-    file.addText(file, "Second")
-    file.addText(file, "Third")
+    file.addText("First")
+    file.addText("Second")
+    file.addText("Third")
 }
 
-fun File.addText(file: File, string: String) {
-    val fileContent = file.readBytes()
-    val randomAccessFile = RandomAccessFile(file, "rw")
+fun File.addText( string: String) {
+    val fileContent = this.readBytes()
+    val randomAccessFile = RandomAccessFile(this, "rw")
     randomAccessFile.seek(0)
     randomAccessFile.writeBytes("${string.lowercase()}\n")
     randomAccessFile.write(fileContent)
