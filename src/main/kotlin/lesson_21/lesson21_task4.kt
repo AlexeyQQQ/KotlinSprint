60aa8103eb21f4475ce498ecb73dcd626598c250
AlexeyQQQ <leadterrier@gmail.com>
2023-12-19 11:12:48 +0300

    KS-21-4 changed function

--- src/main/kotlin/lesson_21/lesson21_task4.kt
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -1,7 +1,6 @@
 package lesson_21
 
 import java.io.File
-import java.io.RandomAccessFile
 
 fun main() {
     val file = File("src/main/kotlin/lesson_21/file_task21.txt")
@@ -10,13 +9,12 @@ fun main() {
     file.addText("First")
     file.addText("Second")
     file.addText("Third")
+    file.addText("Первый")
+    file.addText("Второй")
+    file.addText("Третий")
 }
 
-fun File.addText( string: String) {
-    val fileContent = this.readBytes()
-    val randomAccessFile = RandomAccessFile(this, "rw")
-    randomAccessFile.seek(0)
-    randomAccessFile.writeBytes("${string.lowercase()}\n")
-    randomAccessFile.write(fileContent)
-    randomAccessFile.close()
+fun File.addText(string: String) {
+    val fileContent = "${string.lowercase()}\n" + this.readText()
+    this.writeText(fileContent)
 }
